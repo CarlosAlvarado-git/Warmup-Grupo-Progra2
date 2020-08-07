d4f8db7dde3fa5dccea01be34d6c6d6a2bc3f2d6
CruzdelCid <69279485+CruzdelCid@users.noreply.github.com>
2020-08-07 11:15:26 -0600

    Add files via upload

---
+++ Ejercico2.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+struct puntos
+{
+  int x; 
+  int y; 
+  int z;
+}; 
+
+int main(void) 
+{
+  printf("Almcacen de puntos 3D\n");
+  int i; 
+  struct puntos punto[20]; 
+
+  for(i = 0; i < 20; i++)
+  {
+    printf("\n\nInserte el punto No. %d:\n", i+1); 
+    scanf("%d", &punto[i].x); 
+    scanf("%d", &punto[i].y);
+    scanf("%d", &punto[i].z);
+
+    printf("[%d, %d, %d]", punto[i].x, punto[i].x, punto[i].x); 
+  }
+
+  printf("\n\nPUNTOS ALMACENADOS:\n");
+
+  for(i = 0; i < 20; i++)
+  {  
+    printf("[%d, %d, %d]\n", punto[i].x, punto[i].x, punto[i].x); 
+  }
+
+  return 0;
+}
