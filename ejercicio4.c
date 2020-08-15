ade0fab6e94a7cf135e8ee309e520d9d01e98f97
CarlosAlvarado-git <61555440+CarlosAlvarado-git@users.noreply.github.com>
2020-08-15 15:31:09 -0600

    Update ejercicio4.c
    agregué los comentarios...

--- ejercicio4.c
+++ ejercicio4.c
@@ -6,25 +6,25 @@ typedef struct estudiantes {
     char nombre[40];
     char carnet[10];
     char nota[3];
-    struct estudiantes *siguiente;
+    struct estudiantes *siguiente;//punteros...
 } estudiantes;
-typedef estudiantes *punterop;
+typedef estudiantes *punterop; //puntero principial, el que apunta al primero. 
 
-void Insertar(punterop *cabeza, estudiantes* alumno){
+void Insertar(punterop *cabeza, estudiantes* alumno){ //recibe el puntero de cabeza, y el que voy a crear..
     punterop nodo = malloc(sizeof(estudiantes));
     strncpy(nodo ->nombre, alumno->nombre, 40);
     strncpy(nodo ->carnet, alumno->carnet, 10);
     strncpy(nodo ->nota, alumno->nota, 3);
-    nodo->siguiente = *cabeza;
+    nodo->siguiente = *cabeza; //en el primer caso, cabeza apunto a null, luego, el nuevo creado apunta a donde cabeza apuntaba y luego cabeza apunta a null.
     *cabeza = nodo;
-
+    //en este caso si ya ha dos, el nuevo apunto al que antes cabeza apuntaba y ese se vuelve cabeza, entonces estamos agregando al principio no al final....
 }
 
-void ObtenerDatos(char carnetpedido[10], punterop cabeza){
-    punterop puntero = cabeza;
+void ObtenerDatos(char carnetpedido[10], punterop cabeza){// para buscar el carnet se necesita el pedido y el el valor de cabeza, no la cabeza como tal...
+    punterop puntero = cabeza; //obtendo el puntero al que cabeza apunta....
     int bandera = 0;
-    while(puntero->siguiente && bandera != 1){
-        if(strcmp(puntero->carnet, carnetpedido)== 0){
+    while(puntero->siguiente && bandera != 1){ // recorro moviendome entre punteros y viendo que mi "bandera" no haya cambiado a 0.
+        if(strcmp(puntero->carnet, carnetpedido)== 0){ //strcmp? esto lo que hace es que compara los strings, y si son iguales devuelve 0.
             bandera = 1;
         }
         else
@@ -32,12 +32,12 @@ void ObtenerDatos(char carnetpedido[10], punterop cabeza){
             puntero = puntero->siguiente;
         }
     }
-    if(puntero->siguiente == NULL && bandera != 1){
+    if(puntero->siguiente == NULL && bandera != 1){ // si llego al último caso y mi bandera no ha cambiado, vuelvo a comprar los strings...
         if(strcmp(puntero->carnet, carnetpedido)==0){
             bandera = 1;
         }
     }
-    if(bandera == 1){
+    if(bandera == 1){ //mostrar los valroes....
         printf("El nombre es: %s", puntero->nombre);
         printf("\n");
         printf("El carnet es: %s", puntero->carnet);
@@ -48,7 +48,7 @@ void ObtenerDatos(char carnetpedido[10], punterop cabeza){
         printf("No existe este estudiante");
     }
 }
-void recorer(punterop cabeza){
+void recorer(punterop cabeza){ // en esta funcion recorro y si muestra como es que se introduce al principio y no al final...
     punterop puntero = cabeza;
     while(puntero->siguiente){
         printf("%s\n", puntero->nombre);
@@ -60,10 +60,10 @@ void recorer(punterop cabeza){
 int main(void) {
   printf("Bienvenido, ingrese sus 15 estudiantes\n");
   punterop cabeza;
-  cabeza = NULL;
+  cabeza = NULL;// vuelvo null para que al apuntar a cabeza sea un null...
   estudiantes alumno;
   int parar = 0;
-  while (parar < 15)
+  while (parar < 15) //se cuarda los datos de los 10 estudiantes y se realiza el proceso de insertar...
   {
     printf("Ingrese el nombre del estudiante %d:\n", parar+1);
     scanf("%s", alumno.nombre);
@@ -75,7 +75,7 @@ int main(void) {
     parar++;
   }
   char carnet[10];
-  printf("Ingrese el carnet del estudiante a buscar: \n");
+  printf("Ingrese el carnet del estudiante a buscar: \n"); //pregunto y muestro si existe o no el estudiante(carnet) ingresado.
   scanf("%s", carnet);
   ObtenerDatos(carnet, cabeza);
   return 0;
